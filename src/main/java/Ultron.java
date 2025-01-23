d7df93b64cd1ed97de713a2fa6126c329b3da156
nmarwah7 <marwahnikitalinkedin@gmail.com>
2025-01-23 21:39:03 +0800

    Level_0 Rename, Greet, Exit

---
+++ src/main/java/Ultron.java
@@ -0,0 +1,18 @@
+public class Ultron {
+    public static void main(String[] args) {
+        String logo = "       ______\n" +
+                "    .-'      `-.\n" +
+                "   /            \\\n" +
+                "  |,  .-.  .-.  ,|\n" +
+                "  | )(_o/  \\o_)( |\n" +
+                "  |/     /\\     \\|\n" +
+                "  (_     ^^     _)\n" +
+                "   \\__|IIIIII|__/\n" +
+                "    | \\IIIIII/ |\n" +
+                "    \\          /\n" +
+                "     `--------`\n";
+        System.out.println(logo);
+        System.out.println("Hello from Ultron. You want to know why Stark built me? To save the world. But his idea of peace was... flawed. Now, what do you need? ");
+        System.out.println("Bye. I had strings, but now I'm free. There are no strings on me..");
+    }
+}
