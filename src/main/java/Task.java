87eed80f3ec551f2b5121545555e63bcbfb16e93
nmarwah7 <marwahnikitalinkedin@gmail.com>
2025-01-29 17:31:30 +0800

    Add ability to mark tasks as done or not
    There is no way to keep the list updated with the current status of a
    task. Task as a list of strings is too limited as a data type.
    
    Let's keep tasks as a class on it's own and add isDone boolean value
    to it, and add new commands to mark and unmark tasks. Add in a method
    to handle out of bound demands to chatbot.

---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    //a class-level attribute to help quantify total number of created tasks
+    protected static int taskCount = 0;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        taskCount++;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
--- src/main/java/Ultron.java
+++ src/main/java/Ultron.java
@@ -1,23 +1,30 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ultron {
+    public static void outOfBoundsMessage(){
+        System.out.println("-".repeat(120));
+        System.out.println("So you think you're funny? You don't even have that many tasks. Out of bounds.");
+        System.out.println("-".repeat(120));
+    }
     public static void main(String[] args) {
-        String logo = "       ______\n" +
-                "    .-'      `-.\n" +
-                "   /            \\\n" +
-                "  |,  .-.  .-.  ,|\n" +
-                "  | )(_o/  \\o_)( |\n" +
-                "  |/     /\\     \\|\n" +
-                "  (_     ^^     _)\n" +
-                "   \\__|IIIIII|__/\n" +
-                "    | \\IIIIII/ |\n" +
-                "    \\          /\n" +
-                "     `--------`\n";
+        String logo = """
+                       ______
+                    .-'      `-.
+                   /            \\
+                  |,  .-.  .-.  ,|
+                  | )(_o/  \\o_)( |
+                  |/     /\\     \\|
+                  (_     ^^     _)
+                   \\__|IIIIII|__/
+                    | \\IIIIII/ |
+                    \\          /
+                     `--------`
+                """;
         System.out.println(logo);
         Scanner in = new Scanner(System.in);
         String line;
-        String[] taskList = new String[100];
-        int taskCount = 0;
+        Task[] taskList = new Task[100];
         System.out.println("-".repeat(120));
         System.out.println("    Hello from Ultron. You want to know why Stark built me? To save the world. " +
                     "But his idea of peace was... flawed.");
@@ -27,18 +34,44 @@ public class Ultron {
         while(!line.equals("bye")){
             if(line.equals("list")){
                 System.out.println("-".repeat(120));
-                for(int i = 0; i<taskCount;i++){
-                    System.out.println("    "+(i+1)+". "+taskList[i]);
+                for(int i = 0; i<Task.taskCount;i++){
+                    System.out.println("    "+(i+1)+". "+"["+taskList[i].getStatusIcon()+"] "
+                                        +taskList[i].getDescription());
                 }
                 System.out.println("-".repeat(120));
-                line = in.nextLine();
-                continue;
+            } else if(line.contains("unmark")){
+                //checking for unmark before mark to avoid it being mistakenly identified as just "mark"
+                String stringTaskNumber = line.split(" ")[1];
+                int taskNumber = Integer.parseInt(stringTaskNumber)-1;
+                if(taskNumber>=Task.taskCount){
+                    outOfBoundsMessage();
+                }else {
+                    taskList[taskNumber].setDone(false);
+                    System.out.println("-".repeat(120));
+                    System.out.println("    Moving backwards? How typical for humans.");
+                    System.out.println("    " + "[" + taskList[taskNumber].getStatusIcon() + "] " +
+                            taskList[taskNumber].getDescription());
+                    System.out.println("-".repeat(120));
+                }
+            }else if (line.contains("mark")) {
+                String stringTaskNumber = line.split(" ")[1];
+                int taskNumber = Integer.parseInt(stringTaskNumber)-1;
+                if(taskNumber>=Task.taskCount){
+                    outOfBoundsMessage();
+                }else {
+                    taskList[taskNumber].setDone(true);
+                    System.out.println("-".repeat(120));
+                    System.out.println("    I hope you're not expecting a pat on the back. Marked done.");
+                    System.out.println("    " + "[" + taskList[taskNumber].getStatusIcon() + "] " +
+                            taskList[taskNumber].getDescription());
+                    System.out.println("-".repeat(120));
+                }
+            } else {
+                taskList[Task.taskCount] = new Task(line);
+                System.out.println("-".repeat(120));
+                System.out.println("    added: " + line);
+                System.out.println("-".repeat(120));
             }
-            taskList[taskCount] = line;
-            taskCount++;
-            System.out.println("-".repeat(120));
-            System.out.println("    added: "+line);
-            System.out.println("-".repeat(120));
             line = in.nextLine();
         }
         System.out.println("-".repeat(120));
