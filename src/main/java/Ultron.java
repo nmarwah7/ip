606fc7510c91acec6b99306a11e0336880b0950b
nmarwah7 <marwahnikitalinkedin@gmail.com>
2025-01-29 14:59:42 +0800

    Add echo capability to chatbot
    Chatbot cannot answer or interact with user.
    
    Let's import scanner class from java.util and use it to read in a line
    from user and print it back out in terminal.

--- src/main/java/Ultron.java
+++ src/main/java/Ultron.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ultron {
     public static void main(String[] args) {
         String logo = "       ______\n" +
@@ -12,7 +14,22 @@ public class Ultron {
                 "    \\          /\n" +
                 "     `--------`\n";
         System.out.println(logo);
-        System.out.println("Hello from Ultron. You want to know why Stark built me? To save the world. But his idea of peace was... flawed. Now, what do you need? ");
-        System.out.println("Bye. I had strings, but now I'm free. There are no strings on me..");
+        Scanner in = new Scanner(System.in);
+        String line;
+        System.out.println("-".repeat(120));
+        System.out.println("    Hello from Ultron. You want to know why Stark built me? To save the world. " +
+                    "But his idea of peace was... flawed.");
+        System.out.println("    Now, what do you need? ");
+        System.out.println("-".repeat(120));
+        line = in.nextLine();
+        while(!line.equals("bye")){
+            System.out.println("-".repeat(120));
+            System.out.println("    "+line);
+            System.out.println("-".repeat(120));
+            line = in.nextLine();
+        }
+        System.out.println("-".repeat(120));
+        System.out.println("    Bye. I had strings, but now I'm free. There are no strings on me..");
+        System.out.println("-".repeat(120));
     }
 }
