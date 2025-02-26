39d0bccd953b10457a920f51831eb3b375f2364c
nmarwah7 <marwahnikitalinkedin@gmail.com>
2025-02-26 18:16:29 +0800

    Add find command to allow user to search for keywords
    No way to search for a certain task in a long list.
    
    Let's add in a new method to Tasklist class to handle a find command by
    creating a filtered list.

--- src/main/java/ultron/tasks/Tasklist.java
+++ src/main/java/ultron/tasks/Tasklist.java
@@ -11,12 +11,15 @@ public class Tasklist {
     private static Ui ui = null; // Store Ui instance
 
     private static Parser parser;
-    public Tasklist(Ui ui,  Parser parser) {
+
+    public Tasklist(Ui ui, Parser parser) {
         Tasklist.ui = ui;
         Tasklist.parser = parser;
     }
-    public ArrayList<Task> taskList= new ArrayList<>();
-    public  void handleEvent(String line, ArrayList<Task> taskList, boolean inStoredTask) {
+
+    public ArrayList<Task> taskList = new ArrayList<>();
+
+    public void handleEvent(String line, ArrayList<Task> taskList, boolean inStoredTask) {
         try {
             Ultron.EventParameters parsedParams = parser.getEventParameters(line);
 
@@ -29,17 +32,46 @@ public class Tasklist {
         }
     }
 
-    public  void handleDeadline(String line, ArrayList<Task> taskList, boolean inStoredTask) {
+    public void handleDeadline(String line, ArrayList<Task> taskList, boolean inStoredTask) {
         try {
             Ultron.DeadlineParameters parsedParams = parser.getDeadlineParameters(line);
 
             taskList.add(Task.taskCount, new Deadline(parsedParams.deadlineDescription(), parsedParams.deadlineBy()));
             if (!inStoredTask) {
                 ui.taskAddedMessage(taskList, " deadline ");
-            }        } catch (emptyCommandParameterException|ArrayIndexOutOfBoundsException e) {
+            }
+        } catch (emptyCommandParameterException | ArrayIndexOutOfBoundsException e) {
             ui.deadlineDescriptionErrorMessage();
         }
     }
+
+    public void handleFind(String line, ArrayList<Task> taskList) {
+        try {
+            String find = line.split(" ", 2)[1];
+            if (find.trim().isEmpty()){
+                throw new emptyCommandParameterException();
+            }
+            ArrayList<Task> filteredList = new ArrayList<>();
+            int filteredIndex = 0;
+            for (int i = 0; i < Task.taskCount; i++) {
+                if (taskList.get(i).getDescription().contains(find)) {
+                    filteredList.add(taskList.get(i));
+                    filteredIndex++;
+                }
+            }
+            ui.dashLine();
+            for (int k = 0; k < filteredIndex; k++) {
+
+                System.out.println("    " + (k + 1) + ". " + filteredList.get(k));
+
+            }
+            ui.dashLine();
+        } catch (ArrayIndexOutOfBoundsException| emptyCommandParameterException e) {
+            ui.findIndexErrorMessage();
+        }
+
+    }
+
     public void handleTodo(String line, ArrayList<Task> taskList, boolean inStoredTask) {
         try {
             String todoDescription = parser.getTodoParameters(line);
@@ -48,16 +80,17 @@ public class Tasklist {
             if (!inStoredTask) {
                 ui.taskAddedMessage(taskList, " todo ");
             }
-        } catch (emptyCommandParameterException|ArrayIndexOutOfBoundsException e) {
+        } catch (emptyCommandParameterException | ArrayIndexOutOfBoundsException e) {
             ui.todoDescriptionErrorMessage();
         }
     }
+
     public void handleUnmark(String line, ArrayList<Task> taskList) {
         try {
             int taskNumber = parser.getTaskNumber(line);
-            if(taskNumber>=Task.taskCount||taskNumber<0){
+            if (taskNumber >= Task.taskCount || taskNumber < 0) {
                 ui.outOfBoundsMessage();
-            }else {
+            } else {
                 taskList.get(taskNumber).setDone(false);
                 ui.handleUnmarkErrorMessage(taskList, taskNumber);
             }
@@ -69,26 +102,24 @@ public class Tasklist {
     public void handleMark(String line, ArrayList<Task> taskList) {
         try {
             int taskNumber = parser.getTaskNumber(line);
-            if(taskNumber>=Task.taskCount||taskNumber<0){
+            if (taskNumber >= Task.taskCount || taskNumber < 0) {
                 ui.outOfBoundsMessage();
-            }else {
+            } else {
                 taskList.get(taskNumber).setDone(true);
                 ui.handleMarkMessage(taskList, taskNumber);
             }
-        } catch (NumberFormatException|ArrayIndexOutOfBoundsException|emptyCommandParameterException e) {
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException | emptyCommandParameterException e) {
             ui.errorHandleMarkCommand();
         }
     }
 
 
-
-
     public void handleDelete(String line, ArrayList<Task> taskList) {
         try {
             int taskNumber = parser.getTaskNumber(line);
-            if(taskNumber>=Task.taskCount||taskNumber<0){
+            if (taskNumber >= Task.taskCount || taskNumber < 0) {
                 ui.outOfBoundsMessage();
-            }else {
+            } else {
                 Task.taskCount--;
                 ui.handleDeleteMessage(taskList, taskNumber);
                 taskList.remove(taskNumber);
